f76403fb10c611b4dd01cc94e8c3d0c4809ccdf4
Junior Conte <juniorcnt@gmail.com>
2017-05-11 01:07:23 -0300

    Organização da estrutura do código

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,14 +12,14 @@ angular.module('todoTranslateApp')
 
     var ref = firebase.database().ref().child('todos');
 
-    $scope.todos = $firebaseArray(ref);
     $scope.options = ['pt','en','es','de','fr','it'];
+    $scope.todos = $firebaseArray(ref);
     $scope.lang = $cookies.get('lang') || 'pt';
     $scope.hide = $cookies.get('hide') === 'true' || false;
-    $scope.currentUser = null;
+    $scope.user = null;
 
     $scope.addTodo = function(title, lang, user) {
-      if (!!title) {
+      if (title) {
         $scope.todos.$add({
           done: false,
           title: title,
@@ -32,12 +32,6 @@ angular.module('todoTranslateApp')
       }
     };
 
-    $scope.progress = function() {
-      return $scope.todos.filter(function(todo) {
-        return todo.done;
-      }).length / $scope.todos.length * 100;
-    };
-
     $scope.clean = function() {
       $scope.todos.forEach(function(todo) {
         if (todo.done) {
@@ -46,14 +40,20 @@ angular.module('todoTranslateApp')
       });
     };
 
-    $scope.saveLang = function() {
-      $cookies.put('lang', $scope.lang);
-      tmhDynamicLocale.set($scope.lang);
-      $translate.use($scope.lang);
+    $scope.progress = function() {
+      return $scope.todos.filter(function(todo) {
+        return todo.done;
+      }).length / $scope.todos.length * 100;
     };
 
-    $scope.saveHide = function() {
-      $cookies.put('hide', $scope.hide);
+    $scope.saveLang = function(lang) {
+      $cookies.put('lang', lang);
+      tmhDynamicLocale.set(lang);
+      $translate.use(lang);
+    };
+
+    $scope.saveHide = function(hide) {
+      $cookies.put('hide', hide);
     };
 
     $scope.login = function(provider) {
@@ -74,13 +74,13 @@ angular.module('todoTranslateApp')
 
     $firebaseAuth().$onAuthStateChanged(function(firebaseUser) {
       if (firebaseUser) {
-        $scope.currentUser = {
+        $scope.user = {
           uid: firebaseUser.uid,
           photoURL: firebaseUser.photoURL,
           displayName: firebaseUser.displayName
         };
       } else {
-        $scope.currentUser = null;
+        $scope.user = null;
       }
     });
 
